555e2d59ec51a084c7339c890de8c140b13449cf
sabri baazaoui <baazaouisabri@gmail.com>
2020-11-08 23:43:49 +0100

    simple getMap

---
+++ src/main/java/com/sabrouch/msscbrewery/web/model/SodaDto.java
@@ -0,0 +1,24 @@
+package com.sabrouch.msscbrewery.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+/**
+ * Created by sabrouch.
+ * Date: 11/8/2020
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SodaDto {
+    private UUID id;
+    private String name;
+    private String CocaStyle;
+    private Long upc;
+}
